905775ca3ffda7f21c5966407519a213d6a5bacd
Nathan Wash <nwash57@gmail.com>
2021-01-26 09:57:02 -0600

    updating e steps for pancake motor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -737,8 +737,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-// EXTRUDER CALIBRATION SHOWS 881 INSTEAD OF DEFAULT 837 FOR TITAN EXTRUDER
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 881 }
+// EXTRUDER CALIBRATION SHOWS 871 INSTEAD OF DEFAULT 837 FOR TITAN EXTRUDER
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 871 }
 
 /**
  * Default Max Feed Rate (mm/s)
